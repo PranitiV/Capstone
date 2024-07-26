199f8361ac4e402982a1222c07cf8de19e09d776
Praniti V <praniti2903vakharia@gmail.com>
2024-07-26 13:37:33 +0530

    Created firebase project, created FirebaseConfig and metro.config.ts

---
+++ foundit/FirebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"; 
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBEkEadDPkf_8YhWOF04OX9Nq6_iNqy7SM",
+    authDomain: "authentication-prac-ea88f.firebaseapp.com",
+    projectId: "authentication-prac-ea88f",
+    storageBucket: "authentication-prac-ea88f.appspot.com",
+    messagingSenderId: "712939472832",
+    appId: "1:712939472832:web:fd63bac3672a3c61cd6a25",
+    measurementId: "G-5P6CFP8BW9"
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig); 
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP); 
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);   
