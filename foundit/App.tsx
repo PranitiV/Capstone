7ad96017ed55186470570b13f36cf14a5b31a39a
Praniti V <praniti2903vakharia@gmail.com>
2024-09-10 17:21:23 -0400

    Upload bottom tab added

--- foundit/App.tsx
+++ foundit/App.tsx
@@ -1,57 +1,59 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Alert, StyleSheet} from 'react-native';
-import Home from './app/screens/Home'; 
-import LoginSceen from './app/screens/LoginScreen'; 
-import LogoutScreen from './app/screens/LogoutScreen'; 
+import { Alert, StyleSheet } from 'react-native';
+import Home from './app/screens/Home';
+import LoginSceen from './app/screens/LoginScreen';
+import LogoutScreen from './app/screens/LogoutScreen';
+import Upload from './app/screens/Upload';
 import { useEffect, useState } from 'react';
 import { User, onAuthStateChanged } from 'firebase/auth';
 import { FIREBASE_AUTH } from './FirebaseConfig';
 
-const Stack = createNativeStackNavigator(); 
-const InsideStack = createBottomTabNavigator(); 
+const Stack = createNativeStackNavigator();
+const InsideStack = createBottomTabNavigator();
 
-function InsideLayout(){
+function InsideLayout() {
   return (
-      <InsideStack.Navigator initialRouteName="Details">
-        <InsideStack.Screen name="Details" component={Home} />
-        <InsideStack.Screen 
-          name="LOG OUT" 
-          component={LogoutScreen} 
-          listeners={{
-            tabPress: e => {
-              e.preventDefault(); 
-              Alert.alert("LOG OUT", "Are you sure you want to log out?",[
-                {
-                  text: 'Cancel',
-                  onPress: () => console.log('Cancel Pressed'),
-                  style: 'cancel',
-                },
-                {text: 'Log out', onPress: ()=> FIREBASE_AUTH.signOut()},
-              ]); 
-            }
-          }}
-        />
-      </InsideStack.Navigator>
+    <InsideStack.Navigator initialRouteName="Details">
+      <InsideStack.Screen name="Details" component={Home} />
+      <InsideStack.Screen name="Upload" component={Upload} />
+      <InsideStack.Screen
+        name="LOG OUT"
+        component={LogoutScreen}
+        listeners={{
+          tabPress: e => {
+            e.preventDefault();
+            Alert.alert("LOG OUT", "Are you sure you want to log out?", [
+              {
+                text: 'Cancel',
+                onPress: () => console.log('Cancel Pressed'),
+                style: 'cancel',
+              },
+              { text: 'Log out', onPress: () => FIREBASE_AUTH.signOut() },
+            ]);
+          }
+        }}
+      />
+    </InsideStack.Navigator>
   )
 }
 
 export default function App() {
 
-  const [user, setUser] = useState <User | null>(null)
+  const [user, setUser] = useState<User | null>(null)
 
-  useEffect(()=>{
+  useEffect(() => {
     onAuthStateChanged(FIREBASE_AUTH, (user) => {
-      setUser(user); 
+      setUser(user);
     })
-  },[])
-  
+  }, [])
+
   return (
-    <NavigationContainer> 
+    <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        {user ? 
-          ( <Stack.Screen name="InsideLayout" component ={InsideLayout} options={{headerShown: false}} /> )
+        {user ?
+          (<Stack.Screen name="InsideLayout" component={InsideLayout} options={{ headerShown: false }} />)
           : <Stack.Screen name="Login" component={LoginSceen} />
         }
       </Stack.Navigator>
---
+++ foundit/app/screens/Upload.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Upload = () => {
+    return (
+        <View>
+            <Text> Upload </Text>
+        </View>
+    )
+}
+
+export default Upload
